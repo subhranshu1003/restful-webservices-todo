c96f0d28f015968432ecc2ec1ce16a78359556c7
Subhranshu <subhranshu.pujari@quest-global.com>
2019-08-22 12:42:38 +0530

    first commit

---
+++ src/main/java/com/subh/rest/webservices/restfulwebservices/restfulwebservicestodo/RestfulwebservicesTodoApplication.java
@@ -0,0 +1,14 @@
+package com.subh.rest.webservices.restfulwebservices.restfulwebservicestodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulwebservicesTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulwebservicesTodoApplication.class, args);
+		System.out.println("H2 Db Example");
+	}
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into todo(id, username,description,target_date,is_done)
+values(10001, 'subh', 'Learn JPA', sysdate(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10002, 'subh', 'Learn Data JPA', sysdate(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10003, 'subh', 'Learn Microservices', sysdate(), false);
